e595cac8aa8803b17ee99c995d67e04dec6b074a
Pablo <pablo.bessone@outlook.com>
2020-06-08 16:41:37 -0300

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// settings
+app.set("port", process.env.PORT || 6000);
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use("/users", require("./routes/users"));
+app.use("/notes", require("./routes/notes"));
+
+module.exports = app;
---
+++ backend/src/routes/notes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const router = Router();
+
+router.route("/").get((req, res) => res.send("Notes Routes"));
+// .post();
+router.route("/:id");
+// .get();
+// .put();
+// .delete();
+module.exports = router;
